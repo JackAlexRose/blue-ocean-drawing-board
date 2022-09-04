f8bbde72ceaf5369fd4f5da2a97c90481772487f
JackAlexRose <jackalexanderrose@hotmail.com>
2022-09-04 22:50:54 +0100

    feat: factors form

--- src/components/BlueOceanForm/BlueOceanForm.tsx
+++ src/components/BlueOceanForm/BlueOceanForm.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import styles from "./BlueOceanForm.module.css";
+import { FactorsForm } from "../FactorsForm";
 
 export type BlueOceanFormProps = {
   titleChangeHandler: (title: string) => void;
@@ -17,7 +18,9 @@ const BlueOceanForm: React.FC<BlueOceanFormProps> = ({
         label="Chart Title"
         onChange={(e) => titleChangeHandler(e.target.value)}
         variant="standard"
+        style={{ margin: "20px 20px" }}
       />
+      <FactorsForm />
     </div>
   );
 };
---
+++ src/components/FactorsForm/FactorsForm.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { TextField, Select, Button } from "@mui/material";
+import styles from "./FactorsForm.module.css";
+
+export const FactorsForm = () => {
+  const [factorList, setFactorList] = React.useState<string[]>([]);
+  const [activeFactor, setActiveFactor] = React.useState<number>(-1);
+  const [factorName, setFactorName] = React.useState<string>("");
+
+  React.useEffect(() => {
+    if (activeFactor < 0) {
+      setActiveFactor(factorList.length - 1);
+    }
+  }, []);
+
+  const handleSelectChange = (value: string) => {
+    if (factorList.includes(value)) {
+      setActiveFactor(factorList.indexOf(value));
+    }
+  };
+
+  const addFactor = () => {
+    let newIndex = factorList.length + 1;
+    while (factorList.includes(`Factor ${newIndex}`)) {
+      newIndex++;
+    }
+    const newFactorList = [...factorList, `Factor ${newIndex}`];
+    setFactorList(newFactorList);
+    setActiveFactor(newFactorList.length - 1);
+    setFactorName("");
+  };
+
+  const editFactorName = (value: string) => {
+    if (activeFactor !== undefined) {
+      const newFactorList = [...factorList];
+      newFactorList[activeFactor] = value;
+      setFactorList(newFactorList);
+      setFactorName(value);
+    }
+  };
+
+  const deleteFactor = () => {
+    if (activeFactor !== undefined) {
+      const newFactorList = [...factorList];
+      newFactorList.splice(activeFactor, 1);
+      setFactorList(newFactorList);
+      setActiveFactor(newFactorList.length - 1);
+      setFactorName("");
+    }
+  };
+
+  return (
+    <div className={styles.factorContainer}>
+      <Button
+        variant="contained"
+        color="success"
+        disabled={factorList.length >= 10}
+        style={{ margin: "20px" }}
+        onClick={() => {
+          if (factorList.length < 10) addFactor();
+        }}
+      >
+        {factorList.length < 10 ? "Add Factor" : "Max Reached"}
+      </Button>
+      <Select
+        native
+        onChange={(e) => handleSelectChange(e.target.value as string)}
+        label="test"
+        value={factorList[activeFactor || 0]}
+      >
+        {factorList.map((name, index) => (
+          <option key={index} value={name}>
+            {name}
+          </option>
+        ))}
+      </Select>
+      {activeFactor >= 0 ? (
+        <div>
+          <TextField
+            label="Rename Factor"
+            onChange={(e) => editFactorName(e.target.value)}
+            variant="standard"
+            style={{ margin: "20px 20px" }}
+            value={factorName}
+          ></TextField>
+          <Button
+            variant="contained"
+            color="error"
+            style={{ margin: "20px" }}
+            onClick={deleteFactor}
+          >
+            Delete Factor
+          </Button>
+        </div>
+      ) : null}
+    </div>
+  );
+};
